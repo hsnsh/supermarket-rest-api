70f795d2135e36c84f75006b197be1491c0992b9
Hasan H. SAHIN <hsnsh@outlook.com>
2022-11-14 20:37:54 +0300

    add get tests

--- test/HsNsH.SuperMarket.CatalogService.UnitTests/DomainTests/GenericRepositoryTests.cs
+++ test/HsNsH.SuperMarket.CatalogService.UnitTests/DomainTests/GenericRepositoryTests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using HsNsH.SuperMarket.CatalogService.Domain.Models;
+using HsNsH.SuperMarket.CatalogService.Domain.Shared.Exceptions;
 using HsNsH.SuperMarket.CatalogService.Persistence.Repositories;
 using HsNsH.SuperMarket.CatalogService.UnitTests.DomainTests.TestBase;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +9,121 @@ namespace HsNsH.SuperMarket.CatalogService.UnitTests.DomainTests;
 
 public class GenericRepositoryTests : BaseRepositoryTests
 {
+    #region FindAsync_Tests
+
+    [Fact]
+    public async Task FindAsync_WithUnExistingItem_ReturnsNull()
+    {
+        // Arrange
+        var context = await CreateDefaultContextAsync();
+        var repository = new GenericRepository<CatalogServiceTestDbContext, Product>(context);
+        var refId = Guid.NewGuid();
+
+        // Act
+        var result = await repository.FindAsync(x => x.Id.Equals(refId), includeDetails: false);
+
+        // Assert
+        result.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task FindAsync_WithDuplicateItems_ThrowException()
+    {
+        // Arrange
+        var context = await CreateDefaultContextAsync();
+        var repository = new GenericRepository<CatalogServiceTestDbContext, Product>(context);
+
+        // Act
+        Func<Task> act = async () => await repository.FindAsync(x => x.Name.Contains("Product"), includeDetails: false);
+
+        // Assert
+        var result = await act.Should().ThrowAsync<Exception>();
+        result.And.Message.Should().Contain("Sequence contains more than one element");
+    }
+
+    [Theory]
+    [InlineData("b61cf65c-edfc-4a23-90a8-112fd957fab5", true)]
+    [InlineData("b61cf65c-edfc-4a23-90a8-112fd957fab5", false)]
+    public async Task FindAsync_WithExistingItem_ReturnsExpectedItem(string findId, bool includeDetails)
+    {
+        // Arrange
+        var context = await CreateDefaultContextAsync();
+        var repository = new GenericRepository<CatalogServiceTestDbContext, Product>(context);
+        var refId = Guid.Parse(findId);
+
+        // Act
+        var result = await repository.FindAsync(x => x.Id.Equals(refId), includeDetails: includeDetails);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<Product>();
+
+        if (includeDetails)
+        {
+            result.Category.Should().NotBeNull();
+        }
+    }
+
+    #endregion
+
+    #region GetAsync_Tests
+
+    [Fact]
+    public async Task GetAsync_WithUnExistingItem_ThrowDomainException()
+    {
+        // Arrange
+        var context = await CreateDefaultContextAsync();
+        var repository = new GenericRepository<CatalogServiceTestDbContext, Product>(context);
+        var refId = Guid.NewGuid();
+
+        // Act
+        Func<Task> act = async () => await repository.GetAsync(x => x.Id.Equals(refId), includeDetails: false);
+
+        // Assert
+        var result = await act.Should().ThrowAsync<DomainException>();
+        result.And.Message.Should().Contain("There is no such an entity");
+    }
+
+    [Fact]
+    public async Task GetAsync_WithDuplicateItems_ThrowException()
+    {
+        // Arrange
+        var context = await CreateDefaultContextAsync();
+        var repository = new GenericRepository<CatalogServiceTestDbContext, Product>(context);
+
+        // Act
+        Func<Task> act = async () => await repository.GetAsync(x => x.Name.Contains("Product"), includeDetails: false);
+
+        // Assert
+        var result = await act.Should().ThrowAsync<Exception>();
+        result.And.Message.Should().Contain("Sequence contains more than one element");
+    }
+
+    [Theory]
+    [InlineData("b61cf65c-edfc-4a23-90a8-112fd957fab5", true)]
+    [InlineData("b61cf65c-edfc-4a23-90a8-112fd957fab5", false)]
+    public async Task GetAsync_WithExistingItem_ReturnsExpectedItem(string findId, bool includeDetails)
+    {
+        // Arrange
+        var context = await CreateDefaultContextAsync();
+        var repository = new GenericRepository<CatalogServiceTestDbContext, Product>(context);
+        var refId = Guid.Parse(findId);
+
+        // Act
+        var result = await repository.GetAsync(x => x.Id.Equals(refId), includeDetails: includeDetails);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<Product>();
+
+        if (includeDetails)
+        {
+            result.Category.Should().NotBeNull();
+        }
+    }
+
+    #endregion
+
     #region GetListAsync_Tests
 
     [Theory]
@@ -41,63 +157,6 @@ public class GenericRepositoryTests : BaseRepositoryTests
         if (includeDetails)
         {
             items.Any(x => x.Category != null).Should().Be(true);
-        }
-    }
-
-    #endregion
-
-    #region FindAsync_Tests
-
-    [Fact]
-    public async Task FindAsync_WithUnExistingItem_ReturnsNull()
-    {
-        // Arrange
-        var context = await CreateDefaultContextAsync();
-        var repository = new GenericRepository<CatalogServiceTestDbContext, Product>(context);
-        var refId = Guid.NewGuid();
-
-        // Act
-        var result = await repository.FindAsync(x => x.Id.Equals(refId), includeDetails: false);
-
-        // Assert
-        result.Should().BeNull();
-    }
-
-    [Fact]
-    public async Task FindAsync_WithDuplicateItems_ThrowException()
-    {
-        // Arrange
-        var context = await CreateDefaultContextAsync();
-        var repository = new GenericRepository<CatalogServiceTestDbContext, Product>(context);
-
-        // Act
-        Func<Task> act = async () => await repository.FindAsync(x => x.Name.Contains("Product"), includeDetails: false);
-
-        // Assert
-        var result = await act.Should().ThrowAsync<Exception>();
-        result.And.Message.Should().Be("Sequence contains more than one element");
-    }
-
-    [Theory]
-    [InlineData("b61cf65c-edfc-4a23-90a8-112fd957fab5", true)]
-    [InlineData("b61cf65c-edfc-4a23-90a8-112fd957fab5", false)]
-    public async Task FindAsync_WithExistingItem_ReturnsExpectedItem(string findId, bool includeDetails)
-    {
-        // Arrange
-        var context = await CreateDefaultContextAsync();
-        var repository = new GenericRepository<CatalogServiceTestDbContext, Product>(context);
-        var refId = Guid.Parse(findId);
-
-        // Act
-        var result = await repository.FindAsync(x => x.Id.Equals(refId), includeDetails: includeDetails);
-
-        // Assert
-        result.Should().NotBeNull();
-        result.Should().BeOfType<Product>();
-
-        if (includeDetails)
-        {
-            result.Category.Should().NotBeNull();
         }
     }
 
